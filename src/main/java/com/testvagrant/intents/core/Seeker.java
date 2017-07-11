b335087d71e709d3d9075a1923da95bfa3230a9e
krishnb <kbandame@gmail.com>
2017-07-11 22:07:03 +0530

    Updated Intent creation logic for Performance

--- src/main/java/com/testvagrant/intents/core/Seeker.java
+++ src/main/java/com/testvagrant/intents/core/Seeker.java
@@ -1,19 +1,17 @@
 package com.testvagrant.intents.core;
 
 
-import com.sun.istack.internal.NotNull;
 import com.testvagrant.intents.entities.Elements;
 import com.testvagrant.intents.entities.Feature;
 import com.testvagrant.intents.exceptions.FeatureNotFoundException;
 import com.testvagrant.intents.exceptions.IntentNotFoundException;
 
-import javax.annotation.Nonnull;
 import java.util.List;
 
 public interface Seeker {
 
-    Feature seekFeature(@NotNull List<Feature> features) throws FeatureNotFoundException;
+    Feature seekFeature(List<Feature> features) throws FeatureNotFoundException;
 
-    Elements seekScenario(@Nonnull Feature feature) throws IntentNotFoundException;
+    Elements seekScenario(Feature feature) throws IntentNotFoundException;
 
 }
